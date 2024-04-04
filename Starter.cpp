5567e2c39f1c6ef670f6a49ce8cb52c5c2a40c2a
Sam-R-H <Sam-R-H@github.com>
2024-04-04 15:19:02 -0400

    first commit

---
+++ Starter.cpp
@@ -0,0 +1,26 @@
+/*
+ * Program:  First Laboratory
+ * Author:   Samuel Herman
+ * Date:     1/26/24
+ *
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+
+	float width;
+	float length;
+	float area;
+	length = 4.5;
+	width = 3.35;
+	area = length * width;
+	cout << "Rectangle Area = " << area << "\n";	
+	
+	cout << "Who's on First? \n";
+
+	return 0;
+}
